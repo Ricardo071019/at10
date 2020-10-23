4e39a362eac1d51ea9a4b53c3bc6659640e3177f
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-23 12:14:55 +0100

    Update nove.php and onze.php

---
+++ nove.php
@@ -0,0 +1,18 @@
+<?php 
+
+$minha_multa['carro'] = 'Pálio';
+$minha_multa['valor'] = 178.00;
+//alteração de valores
+$minha_multa['carro'] = ' ED 1.0';
+$minha_multa['valor'] += 20;
+//exibe o aray
+var_dump($minha_multa);
+echo '<br>';
+$comidas[] = 'Lazanha';
+$comidas[] = 'Pizza';
+$comidas[] = 'Macarrão';
+//alteração d valores
+$comidas[1] = 'Pizza Calabreza';
+//exibe o array
+var_dump($comidas);
+?>
---
+++ onze.php
@@ -0,0 +1,32 @@
+<?php
+
+$carros = array (
+	'Punto'=> array(
+ 		'cor' => 'azul',
+ 		'potencia' => '1.0',
+ 		'opcionais' => 'Ar Cond.'
+	),
+	'Corsa' => array(
+		'cor' => 'cinza',
+ 		'potencia' => '1.3',
+ 		'opcionais' => 'MP3.'
+	),
+	'Golf' => array(
+		'cor' => 'branco',
+ 		'potencia' => '1.0',
+ 		'opcionais' => 'Metalica.'
+
+	)
+);
+
+echo $carros['punto']['opcionais'];
+
+foreach($carros as $modelo =>$carro)
+{
+	echo '<h1>' .$modelo. '</h1>';
+	foreach($carros as $chave => $detalhe)
+	{
+		echo '<b>' .$chave. '</b>' .$detalhe. '<br>';
+	}
+}
+?>
